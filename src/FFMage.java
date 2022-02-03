67a9d29ac608b1551d448732e271077d934a1f67
lebrancconvas <lebrancconvas@gmail.com>
2022-02-03 14:16:41 +0700

    👔 Update Mage Class.

--- src/FFMage.java
+++ src/FFMage.java
@@ -1,3 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+
+
 class Mage
 {
 	String name;
@@ -31,6 +36,28 @@ class WhiteMage extends Mage
 	{
 		super(Name, Level, HP, ATK, DEF, MP, EXP);
 	}
+
+	private String[] skillset = {"Cure", "Revive", "Buff", "Dia", "Debuff"};
+
+	public void useSkill(String skill)
+	{
+		if(Arrays.asList(skillset).contains(skill))
+		{
+			if(mp > 0)
+			{
+				System.out.printf("%s uses %s\n", name, skill);
+				mp--;
+			}
+			else
+			{
+				System.out.printf("%s doesn't have enough MP.\n", name);
+			}
+		}
+		else
+		{
+			System.out.printf("%s doesn't have the capability to use %s.\n", name, skill);
+		}
+	}
 	
 }
 
@@ -47,9 +74,35 @@ class RedMage extends Mage
 class BlueMage extends Mage
 {
 
+	private String[] skillset = {};
+
 	public BlueMage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) 
 	{
 		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+
+	public void absorbMagic(String enemyMagic)
+	{
+		
+	}
+
+	public void useSkill(String skill)
+	{
+		if(Arrays.asList(skillset).contains(skill))
+		{
+			if(mp > 0)
+			{
+				System.out.printf("%s uses %s\n", name, skill);
+			}
+			else
+			{
+				System.out.printf("%s doesn't have enough MP.\n", name);
+			}
+		}
+		else
+		{
+			System.out.printf("%s doesn't have the capability to use %s\n", name, skill);
+		}
 	}
 	
 }
@@ -57,9 +110,31 @@ class BlueMage extends Mage
 class Summoner extends Mage
 {
 
+	private String[] beastlist = {"Ramuh", "Shiva", "Omega Weapon", "Knight of Round", "Bahamut", "Yojimbo"};
+
 	public Summoner(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) 
 	{
 		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+
+	public void beastSummon(String beast)
+	{
+		if(Arrays.asList(beastlist).contains(beast))
+		{
+			if(mp > 0)
+			{
+				System.out.printf("%s summons %s\n", name, beast);
+				mp -= 2;
+			}
+			else
+			{
+				System.out.printf("%s doesn't have enough MP.\n", name);
+			}
+		}
+		else
+		{
+			System.out.printf("%s doesn't have the capability to summon %s\n", name, beast);
+		}
 	}
 	
 }
@@ -68,7 +143,18 @@ public class FFMage
 {
 	public static void main(String[] args)
 	{
-		Mage vivi = new Mage("Vivi", 5, 300, 200, 100, 35, 12);
-		System.out.println(vivi.level);
+		WhiteMage prisma = new WhiteMage("Prisma", 5, 100, 100, 200, 2, 200);
+		prisma.useSkill("Cure");
+		prisma.useSkill("Curaga");
+		prisma.useSkill("Dia");
+		prisma.useSkill("Buff");
+		prisma.useSkill("Thunder");
+
+		Summoner yuna = new Summoner("Yuna", 10, 300, 300, 200, 10, 200);
+		yuna.beastSummon("Ramuh");
+		yuna.beastSummon("Ifrit");
+
+		BlueMage sith = new BlueMage("Sith", 10, 500, 250, 450, 20, 300);
+		
 	}
 }
