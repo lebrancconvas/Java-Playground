cc5232c4c75b333e2682f7281ddf29840e5fa727
lebrancconvas <lebrancconvas@gmail.com>
2022-02-03 07:09:30 +0700

    ✨ Create Final Fantasy Mage Simulation Program.

---
+++ src/FFMage.java
@@ -0,0 +1,60 @@
+class Mage
+{
+	String name;
+	int level,hp, atk, def, mp, exp;
+
+	public Mage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP)
+	{
+		name = Name;
+		level = Level;
+		hp = HP;
+		atk = ATK;
+		def = DEF;
+		mp = MP;
+		exp = EXP;
+	}
+}
+
+class BlackMage extends Mage
+{
+	public BlackMage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) {
+		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+	
+}
+
+class WhiteMage extends Mage
+{
+
+	public WhiteMage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) {
+		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+	
+}
+
+class RedMage extends Mage
+{
+
+	public RedMage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) {
+		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+	
+}
+
+class BlueMage extends Mage
+{
+
+	public BlueMage(String Name, int Level, int HP, int ATK, int DEF, int MP, int EXP) {
+		super(Name, Level, HP, ATK, DEF, MP, EXP);
+	}
+	
+}
+
+public class FFMage 
+{
+	public static void main(String[] args)
+	{
+		Mage vivi = new Mage("Vivi", 5, 300, 200, 100, 35, 12);
+		System.out.println(vivi.level);
+	}
+}
